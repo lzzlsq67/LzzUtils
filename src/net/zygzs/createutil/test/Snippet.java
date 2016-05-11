d4c4207770f8836c952dee298550d119c810ad49
刘泽中 <29013329@qq.com>
2016-05-11 14:27:36 +0800

    1

---
+++ src/net/zygzs/createutil/test/Snippet.java
@@ -0,0 +1,30 @@
+package net.zygzs.createutil.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.zygzs.createutil.createBean.CreateBeanUtil;
+import net.zygzs.createutil.createBean.bean.BeanData;
+
+public class Snippet {
+
+	public static void main( String[] args ) {
+//		String className = "ReconciliationBatch";
+//		List<BeanData> datas = new ArrayList<BeanData>();
+//		datas.add( new BeanData( "id","Long", "自增长ID" ) );
+//		datas.add( new BeanData( "version", "Long", "版本号" ) );
+//		datas.add( new BeanData( "create_time", "Date", "创建时间" ) );
+//		datas.add( new BeanData( "update_time", "Date", "修改时间" ) );
+//		datas.add( new BeanData( "rec_date","String", "对账日期" ) );
+//		datas.add( new BeanData( "rec_batch", "Long", "对账批次" ) );
+//		datas.add( new BeanData( "bypay_transactions_total", "Long", "平台交易总笔数" ) );
+//		datas.add( new BeanData( "bypay_amount_total", "Long", "平台总金额(元)" ) );
+//		datas.add( new BeanData( "bank_transactions_total", "Long", "银行交易总笔数" ) );
+//		datas.add( new BeanData( "bankl_amount_total", "Long", "银行总金额(元)" ) );
+//		datas.add( new BeanData( "rec_result", "Integer", "对帐结果 0:对帐中 1:正常 2:异常" ) );
+//		CreateBeanUtil.printBean( className, datas );
+		List<String> strs= null;
+		int no = strs.size();
+		System.out.println( no );
+	}
+}
